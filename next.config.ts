ea0dbecd96dbe3ef8917f5d127fe9003a975742c
Siva Charan Harimanikyam <sivacharanhariman@gmail.com>
2026-06-08 15:04:15 -0400

    Fix Vercel deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/rossy-cookies-website",
-  assetPrefix: "/rossy-cookies-website/",
   images: {
     unoptimized: true,
   },
